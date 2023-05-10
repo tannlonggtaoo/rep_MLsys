0e2480a2602c9cb939b6d3346ddb0c352be20f7c
tannlonggtaoo <tlt19@mails.tsinghua.edu.cn>
2023-05-10 17:07:55 +0800

    add resnet for dorefanet and mobilenet for pact

--- pact/train_mobilenetv2.py
+++ pact/train_mobilenetv2.py
@@ -2,8 +2,8 @@ import torch
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from resnet20 import NaiveResnet20
+from torch.utils.data import DataLoader, ConcatDataset
+from mobilenetv2 import NaiveMobileNetV2
 import os
 import re
 import numpy as np
@@ -38,16 +38,21 @@ def getloader(batch_size, num_workers, mode='both'):
     if mode == 'test':
         return testloader
     
-def train(model, device, train_loader, optimizer, epoch, loss_fn):
+def train(model, device, train_loader, optimizer, epoch, loss_fn, lamda):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = loss_fn(output, target)
+        reg = 0.0
+        for name,param in model.named_parameters():
+            if "alpha" in name:
+                reg += torch.pow(param, 2)
+        loss += lamda * reg
         loss.backward()
         optimizer.step()
-        if(batch_idx + 1)%(len(train_loader) // 4) == 0: 
+        if(batch_idx + 1)%(len(train_loader) // 3) == 0: 
             print(f'[Train] Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}] Loss: {loss.item():.6f}')
 
 def test(model, device, test_loader, loss_fn = nn.CrossEntropyLoss(reduction='sum'), verbose = True):
@@ -66,7 +71,7 @@ def test(model, device, test_loader, loss_fn = nn.CrossEntropyLoss(reduction='su
     test_loss /= len(test_loader.dataset)
     if verbose:
         print(f'[Test] Average loss: {test_loss:.3f} Accuracy: {correct}/{len(test_loader.dataset)} ({100 * correct / len(test_loader.dataset):.3f}%)\n')
-    return test_loss
+    return test_loss, correct / len(test_loader.dataset)
 
 def loadparam(ckpt_path):
     files = os.listdir(ckpt_path)
@@ -76,43 +81,44 @@ def loadparam(ckpt_path):
     ckpt = torch.load(load_path)
     return ckpt, begin_epoch
 
-def resnet_examine(bitW,bitA,bitG):
+def pact_examine(bitW,bitA):
 
-    batch_size = 256 # a little different
-    n_epoch = 40
-    num_workers = 8
+    batch_size = 96 # a little different
+    n_epoch = 50
+    num_workers = 4
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_loader, test_loader = getloader(batch_size,num_workers)
 
-    model = NaiveResnet20(n_class=10,bitW=bitW,bitA=bitA,bitG=bitG)
+    model = NaiveMobileNetV2(n_class=10,bitW=bitW,bitA=bitA)
     model.to(device)
     resume_if_ckpt_exists = False
     begin_epoch = 0 # will be overwritten if previous ckpt is loaded
-    ckpt_path = './dorefanet/models/resnet20'
+    ckpt_path = './pact/models/mobilenetv2'
 
     if (resume_if_ckpt_exists):
         ckpt, begin_epoch = loadparam(ckpt_path)
         model.load_state_dict(ckpt)
 
-    folder_path = os.path.join(ckpt_path, f'W{bitW}_A{bitA}_G{bitG}')
+    folder_path = os.path.join(ckpt_path, f'W{bitW}_A{bitA}')
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
 
     optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20])
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', verbose=True)
     loss_fn = nn.CrossEntropyLoss()
     loss_fn_test = nn.CrossEntropyLoss(reduction='sum')
+    lamda = 0.0002
 
     for epoch in range(begin_epoch, begin_epoch + n_epoch):
-        train(model, device, train_loader, optimizer, epoch, loss_fn)
-        testloss = test(model, device, test_loader, loss_fn_test)
+        train(model, device, train_loader, optimizer, epoch, loss_fn, lamda)
+        testloss, acc = test(model, device, test_loader, loss_fn_test)
         
-        torch.save(model.state_dict(), os.path.join(folder_path, f'epoch{epoch}_loss{testloss:.6f}.pth'))
+        torch.save(model.state_dict(), os.path.join(folder_path, f'epoch{epoch}_loss{testloss:.6f}_acc{100 * acc:.3f}.pth'))
         print(f"[sys] Model saved @ epoch {epoch}...\n")
-        scheduler.step()
+        scheduler.step(testloss)
 
 if __name__ == "__main__":
-    for bitW,bitA,bitG in [[2,2,2],[6,6,6]]:
-        print(f'[sys] bitW {bitW}, bitA {bitA}, bitG {bitG} ------------------------------------')
-        resnet_examine(bitW,bitA,bitG)
+    for bitW,bitA in [[6,6],[5,5],[4,4],[3,3],[2,2]]:
+        print(f'[sys] bitW {bitW}, bitA {bitA} ------------------------------------')
+        pact_examine(bitW,bitA)
--- pact/train_resnet20.py
+++ pact/train_resnet20.py
@@ -2,7 +2,7 @@ import torch
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 from resnet20 import NaiveResnet20
 import os
 import re
@@ -38,16 +38,21 @@ def getloader(batch_size, num_workers, mode='both'):
     if mode == 'test':
         return testloader
     
-def train(model, device, train_loader, optimizer, epoch, loss_fn):
+def train(model, device, train_loader, optimizer, epoch, loss_fn, lamda):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = loss_fn(output, target)
+        reg = 0.0
+        for name,param in model.named_parameters():
+            if "alpha" in name:
+                reg += torch.pow(param, 2)
+        loss += lamda * reg
         loss.backward()
         optimizer.step()
-        if(batch_idx + 1)%(len(train_loader) // 4) == 0: 
+        if(batch_idx + 1)%(len(train_loader) // 3) == 0: 
             print(f'[Train] Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)}] Loss: {loss.item():.6f}')
 
 def test(model, device, test_loader, loss_fn = nn.CrossEntropyLoss(reduction='sum'), verbose = True):
@@ -76,36 +81,37 @@ def loadparam(ckpt_path):
     ckpt = torch.load(load_path)
     return ckpt, begin_epoch
 
-def resnet_examine(bitW,bitA,bitG):
+def pact_examine(bitW,bitA):
 
-    batch_size = 256 # a little different
-    n_epoch = 40
-    num_workers = 8
+    batch_size = 128 # a little different
+    n_epoch = 140
+    num_workers = 4
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_loader, test_loader = getloader(batch_size,num_workers)
 
-    model = NaiveResnet20(n_class=10,bitW=bitW,bitA=bitA,bitG=bitG)
+    model = NaiveResnet20(n_class=10,bitW=bitW,bitA=bitA)
     model.to(device)
     resume_if_ckpt_exists = False
     begin_epoch = 0 # will be overwritten if previous ckpt is loaded
-    ckpt_path = './dorefanet/models/resnet20'
+    ckpt_path = './pact/models/resnet20'
 
     if (resume_if_ckpt_exists):
         ckpt, begin_epoch = loadparam(ckpt_path)
         model.load_state_dict(ckpt)
 
-    folder_path = os.path.join(ckpt_path, f'W{bitW}_A{bitA}_G{bitG}')
+    folder_path = os.path.join(ckpt_path, f'W{bitW}_A{bitA}')
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
 
     optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20])
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 120])
     loss_fn = nn.CrossEntropyLoss()
     loss_fn_test = nn.CrossEntropyLoss(reduction='sum')
+    lamda = 0.0002
 
     for epoch in range(begin_epoch, begin_epoch + n_epoch):
-        train(model, device, train_loader, optimizer, epoch, loss_fn)
+        train(model, device, train_loader, optimizer, epoch, loss_fn, lamda)
         testloss = test(model, device, test_loader, loss_fn_test)
         
         torch.save(model.state_dict(), os.path.join(folder_path, f'epoch{epoch}_loss{testloss:.6f}.pth'))
@@ -113,6 +119,7 @@ def resnet_examine(bitW,bitA,bitG):
         scheduler.step()
 
 if __name__ == "__main__":
-    for bitW,bitA,bitG in [[2,2,2],[6,6,6]]:
-        print(f'[sys] bitW {bitW}, bitA {bitA}, bitG {bitG} ------------------------------------')
-        resnet_examine(bitW,bitA,bitG)
+    #for bitW,bitA in [[6,6],[5,5],[4,4],[3,3],[2,2]]:
+    for bitW,bitA in [[6,6]]:
+        print(f'[sys] bitW {bitW}, bitA {bitA} ------------------------------------')
+        pact_examine(bitW,bitA)
